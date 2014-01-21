313063d969d8f16d0765ad81021f4bf773cfa7a5
Shamis Shukoor <shamis.shukoor@visualiq.com>
2014-01-21 23:09:01 +0530

    Training table top of content

--- training.php
+++ training.php
@@ -13,42 +13,8 @@
 			</div>
 			<div class="pricing-box">
 				<div class="container">
-	
-					<div class="row">
-
-						<div class="col-md-4">
-							<h4>DEVELOPER</h4>
-							<p>These are short and precise ads which aim completely aimed at brand reinforcements. The ads can be in 
-								the form of a tag line or a jingle of 5 seconds. Hence the gold segment ads of voice flyers are 
-								suitable for those brands which are already familiar with the customers, but a continuous 
-								reinforcement of which would elevate the brand recall and hence influence the customers at the 
-								time of purchase or selection of a service.
-							</p>
-						</div>
-
-						<div class="col-md-4">
-							<h4>ADMINISTRATOR</h4>
-							<p>The Ads in the diamond model are 10 seconds ads. Together with the reinforcement of the brands of 
-								the clients, these ads also aim at communicating a single line of content to the customers apart 
-								from the recall and reinforcement. It can be a very brief introduction of a new product, a new 
-								outlet or maybe even an offer. Hence, these ads play a dual purpose of reinforcement and communication 
-								with the customers.
-							</p>
-						</div>
-
-						<div class="col-md-4">
-							<h4>DATA SCIENCE</h4>
-							<p>Platinum ads are 15 seconds ads which mainly aim at communicating a content with the customers more 
-								than reinforcing the brand. It can be a detailing into an offer for a limited period of time, or 
-								guiding the customers for more information into a client outlet or to the client’s website or even 
-								communicating the benefits of a service offered by the client to the customers. Hence, the initiation 
-								of a detailed enquiry by the customers to the client’s offerings is what is desired.
-							</p>
-						</div>
-
-					</div>
-					<br><br>
-					<h3>More Details</h3>
+				<h3>Courses</h3>
+					
 					<div class="row">
 
 						<div class="col-md-4">
@@ -72,7 +38,7 @@
 									<p>Tag Line or a Jingle</p>
 								</li>
 								<li>
-                                    <a href="#">Register Now</a>
+                                    <a href="#developer">Learn More</a>
                                 </li>
 							</ul>
 						</div>
@@ -129,6 +95,44 @@
 							</ul>
 						</div>
 					</div>
+
+					<div class="row">
+
+						<div class="col-md-4">
+							<span class="anchor" id="developer"></span>
+							<h4>DEVELOPER</h4>
+							<p>Complete overview of Hadoop, it’s components and implementation details. This course will go into 
+								more detail and be tailored more to the real world than other Hadoop course offered. The instruction 
+								is led by seasoned software engineers who have experience developing real work Big Data projects. 
+								Goal is for the student to gain a thorough understanding of the Hadoop ecosystem, basic operational 
+								considerations provide them with knowledge of how to develop applications using Hadoop with Java and 
+								finally provide working knowledge of important advanced topics.
+							</p>
+						</div>
+
+						<div class="col-md-4">
+							<h4>ADMINISTRATOR</h4>
+							<p>The Ads in the diamond model are 10 seconds ads. Together with the reinforcement of the brands of 
+								the clients, these ads also aim at communicating a single line of content to the customers apart 
+								from the recall and reinforcement. It can be a very brief introduction of a new product, a new 
+								outlet or maybe even an offer. Hence, these ads play a dual purpose of reinforcement and communication 
+								with the customers.
+							</p>
+						</div>
+
+						<div class="col-md-4">
+							<h4>DATA SCIENCE</h4>
+							<p>Platinum ads are 15 seconds ads which mainly aim at communicating a content with the customers more 
+								than reinforcing the brand. It can be a detailing into an offer for a limited period of time, or 
+								guiding the customers for more information into a client outlet or to the client’s website or even 
+								communicating the benefits of a service offered by the client to the customers. Hence, the initiation 
+								of a detailed enquiry by the customers to the client’s offerings is what is desired.
+							</p>
+						</div>
+
+					</div>
+					<br><br>
+
 				</div>
 			</div>
 		</div>
