baad461627a373c6fe582b3faefa7932175e5a1f
Shamis Shukoor <shamis.shukoor@visualiq.com>
2014-01-25 02:51:15 +0530

    Reverting to the rows

--- training.php
+++ training.php
@@ -89,162 +89,150 @@
 <hr>
 <br>
 <div class="panel-group" id="accordion">
-   <div class="row">
-      <div class="col-md-12">
-         <span class="anchor" id="developer"></span>
-         <div class="panel panel-default">
-            <div class="panel-heading">
-               <h4 class="panel-title">
-                  <a data-toggle="collapse" data-parent="#accordion" href="#developer">
-                  DEVELOPER
-                  </a>
-               </h4>
-               <div class="trainingNavRight">
-                  <a href="#"><span><i class="fa fa-pencil-square-o"></i>Register</span></a>
-                  <a href="files/Developer Course.pdf" target="_blank"><span><i class="fa fa-list "></i>Table of Contents</span></a>
-                  <a data-toggle="modal" href="#devloperInstructor"><span><i class="fa fa-user"></i>Meet the Instructor</span></a>
-               </div>
-            </div>
-            <div id="developer" class="panel-collapse collapse in">
-               <div class="panel-body">
-                  <h5>Course Overview : </h5>
-                  <p>Complete overview of Hadoop, it’s components and implementation details. This course will go into 
-                     more detail and be tailored more to the real world than other Hadoop course offered. The instruction 
-                     is led by seasoned software engineers who have experience developing real work Big Data projects.
-                  </p>
-                  <br>
-                  <h5>Objectives : </h5>
-                  <p>Goal is for the student to gain a thorough understanding of the Hadoop ecosystem, basic operational 
-                     considerations provide them with knowledge of how to develop applications using Hadoop with Java and 
-                     finally provide working knowledge of important advanced topics.
-                  </p>
-                  <br>
-                  <h5>Lab Overview : </h5>
-                  <p>The lab is a student tailored project that will bring together the various concepts learned during the course. 
-                     Together with the instructor the student will design a project that can use an existing or one their own 
-                     data sets. This project concept will be determined at the beginning of class, allowing the student to take 
-                     the concepts taught and apply them to a project tailored to their need/liking.
-                  </p>
-                  <br>
-                  <h5>Prerequisite &amp; Target Audience : </h5>
-                  <p>This course is designed for Developers, students and technical managers who want to learn to implement 
-                     Hadoop MapReduce jobs or manage Big data projects. The course is ideal for experienced software developers 
-                     who have experience in traditional data management technologies and want to upgrade their skills.
-                  </p>
-                  <br>
-                  <h5>Duration : </h5>
-                  <p>4-5 Days OR 32-40 Hours</p>
-                  <br>
-                  <h5>Price : </h5>
-               </div>
-            </div>
+   <div class="panel panel-default">
+      <div class="panel-heading">
+         <h4 class="panel-title">
+            <a data-toggle="collapse" data-parent="#accordion" href="#developer">
+            DEVELOPER
+            </a>
+         </h4>
+         <div class="trainingNavRight">
+            <a href="#"><span><i class="fa fa-pencil-square-o"></i>Register</span></a>
+            <a href="files/Developer Course.pdf" target="_blank"><span><i class="fa fa-list "></i>Table of Contents</span></a>
+            <a data-toggle="modal" href="#devloperInstructor"><span><i class="fa fa-user"></i>Meet the Instructor</span></a>
+         </div>
+      </div>
+      <div id="developer" class="panel-collapse collapse in">
+         <div class="panel-body">
+            <h5>Course Overview : </h5>
+            <p>Complete overview of Hadoop, it’s components and implementation details. This course will go into 
+               more detail and be tailored more to the real world than other Hadoop course offered. The instruction 
+               is led by seasoned software engineers who have experience developing real work Big Data projects.
+            </p>
+            <br>
+            <h5>Objectives : </h5>
+            <p>Goal is for the student to gain a thorough understanding of the Hadoop ecosystem, basic operational 
+               considerations provide them with knowledge of how to develop applications using Hadoop with Java and 
+               finally provide working knowledge of important advanced topics.
+            </p>
+            <br>
+            <h5>Lab Overview : </h5>
+            <p>The lab is a student tailored project that will bring together the various concepts learned during the course. 
+               Together with the instructor the student will design a project that can use an existing or one their own 
+               data sets. This project concept will be determined at the beginning of class, allowing the student to take 
+               the concepts taught and apply them to a project tailored to their need/liking.
+            </p>
+            <br>
+            <h5>Prerequisite &amp; Target Audience : </h5>
+            <p>This course is designed for Developers, students and technical managers who want to learn to implement 
+               Hadoop MapReduce jobs or manage Big data projects. The course is ideal for experienced software developers 
+               who have experience in traditional data management technologies and want to upgrade their skills.
+            </p>
+            <br>
+            <h5>Duration : </h5>
+            <p>4-5 Days OR 32-40 Hours</p>
+            <br>
+            <h5>Price : </h5>
          </div>
       </div>
    </div>
-   <div class="row">
-      <div class="col-md-12">
-         <span class="anchor" id="administrator"></span>
-         <div class="panel panel-default">
-            <div class="panel-heading">
-               <h4 class="panel-title">
-                  <a data-toggle="collapse" data-parent="#accordion" href="#administrator">
-                  ADMINISTRATOR
-                  </a>
-               </h4>
-               <div class="trainingNavRight">
-                  <a href="#"><span><i class="fa fa-pencil-square-o"></i>Register</span></a>
-                  <a href="files/Admin Course.pdf" target="_blank"><span><i class="fa fa-list "></i>Table of Contents</span></a>
-                  <a data-toggle="modal" href="#devloperInstructor"><span><i class="fa fa-user"></i>Meet the Instructor</span></a>
-               </div>
-            </div>
-            <div id="administrator" class="panel-collapse collapse in">
-               <div class="panel-body">
-                  <h5>Course Overview : </h5>
-                  <p>The course covers the fundamental concepts of Apache Hadoop and it’s ecosystem. 
-                     It covers how to deploy, manage, monitor, and secure a Hadoop Cluster using Cloudera manager/HortonWorks 
-                     Sandbox and Ambari. It also covers backups, high availability, federation and possible disaster recovery 
-                     scenarios. In Hadoop’s echo system, we will cover basics about HBase, Pig, Flume, Hive, Impala, Sqoop 
-                     and Oozie. Guest lectures and practical labs including real life projects provide skills to succeed 
-                     in challenging work environment.
-                  </p>
-                  <br>
-                  <h5>Objectives : </h5>
-                  <p>Basics of Hadoop along with it’s Ecosystem. HDFS and MapReduce. Hadoop cluster – setup, maintenance, 
-                     monitoring, troubleshooting and recovery from failure. Load balancing, High availability &amp; Federation.
-                     Hardware and network requirements and optimal configurations. Kerberos-based security for Hadoop cluster.
-                     Ecosystem - HBase, Pig, Flume, Hive, Impala, Sqoop and Oozie.
-                  </p>
-                  <br>
-                  <h5>Lab Overview : </h5>
-                  <p>Each student set up a pseudo distributed single node cluster on their laptops. In addition, students 
-                     will be shown how to run a larger ~4 to 8 node cluster in Rackspace along with real life data projects. 
-                     The structure will consist of about 40% labs and 60% lecture. Hands-on exercises will be done throughout 
-                     the course.
-                  </p>
-                  <br>
-                  <h5>Prerequisite &amp; Target Audience : </h5>
-                  <p>This course is designed for IT administrators and developers with some basic programming skills and 
-                     knowledge of Linux. Existing knowledge of Hadoop is not required.
-                  </p>
-                  <br>
-                  <h5>Duration : </h5>
-                  <p>4-5 Days OR 32-40 Hours</p>
-                  <br>
-                  <h5>Price : </h5>
-               </div>
-            </div>
+
+   <div class="panel panel-default">
+      <div class="panel-heading">
+         <h4 class="panel-title">
+            <a data-toggle="collapse" data-parent="#accordion" href="#administrator">
+            ADMINISTRATOR
+            </a>
+         </h4>
+         <div class="trainingNavRight">
+            <a href="#"><span><i class="fa fa-pencil-square-o"></i>Register</span></a>
+            <a href="files/Admin Course.pdf" target="_blank"><span><i class="fa fa-list "></i>Table of Contents</span></a>
+            <a data-toggle="modal" href="#devloperInstructor"><span><i class="fa fa-user"></i>Meet the Instructor</span></a>
+         </div>
+      </div>
+      <div id="administrator" class="panel-collapse collapse in">
+         <div class="panel-body">
+            <h5>Course Overview : </h5>
+            <p>The course covers the fundamental concepts of Apache Hadoop and it’s ecosystem. 
+               It covers how to deploy, manage, monitor, and secure a Hadoop Cluster using Cloudera manager/HortonWorks 
+               Sandbox and Ambari. It also covers backups, high availability, federation and possible disaster recovery 
+               scenarios. In Hadoop’s echo system, we will cover basics about HBase, Pig, Flume, Hive, Impala, Sqoop 
+               and Oozie. Guest lectures and practical labs including real life projects provide skills to succeed 
+               in challenging work environment.
+            </p>
+            <br>
+            <h5>Objectives : </h5>
+            <p>Basics of Hadoop along with it’s Ecosystem. HDFS and MapReduce. Hadoop cluster – setup, maintenance, 
+               monitoring, troubleshooting and recovery from failure. Load balancing, High availability &amp; Federation.
+               Hardware and network requirements and optimal configurations. Kerberos-based security for Hadoop cluster.
+               Ecosystem - HBase, Pig, Flume, Hive, Impala, Sqoop and Oozie.
+            </p>
+            <br>
+            <h5>Lab Overview : </h5>
+            <p>Each student set up a pseudo distributed single node cluster on their laptops. In addition, students 
+               will be shown how to run a larger ~4 to 8 node cluster in Rackspace along with real life data projects. 
+               The structure will consist of about 40% labs and 60% lecture. Hands-on exercises will be done throughout 
+               the course.
+            </p>
+            <br>
+            <h5>Prerequisite &amp; Target Audience : </h5>
+            <p>This course is designed for IT administrators and developers with some basic programming skills and 
+               knowledge of Linux. Existing knowledge of Hadoop is not required.
+            </p>
+            <br>
+            <h5>Duration : </h5>
+            <p>4-5 Days OR 32-40 Hours</p>
+            <br>
+            <h5>Price : </h5>
          </div>
       </div>
    </div>
-   <div class="row">
-      <div class="col-md-12">
-         <span class="anchor" id="datascience"></span>
-         <div class="panel panel-default">
-            <div class="panel-heading">
-               <h4 class="panel-title">
-                  <a data-toggle="collapse" data-parent="#accordion" href="#datascience">
-                  DATA SCIENCE
-                  </a>
-               </h4>
-               <div class="trainingNavRight">
-                  <a href="#"><span><i class="fa fa-pencil-square-o"></i>Register</span></a>
-                  <a href="files/Data Science Course.pdf" target="_blank"><span><i class="fa fa-list "></i>Table of Contents</span></a>
-                  <a data-toggle="modal" href="#devloperInstructor"><span><i class="fa fa-user"></i>Meet the Instructor</span></a>
-               </div>
-            </div>
-            <div id="datascience" class="panel-collapse collapse in">
-               <div class="panel-body">
-                  <h5>Course Overview : </h5>
-                  <p>Complete overview of Hadoop, it’s components and implementation details. This course will go into 
-                     more detail and be tailored more to the real world than other Hadoop course offered. The instruction 
-                     is led by seasoned software engineers who have experience developing real work Big Data projects.
-                  </p>
-                  <br>
-                  <h5>Objectives : </h5>
-                  <p>Goal is for the student to gain a thorough understanding of the Hadoop ecosystem, basic operational 
-                     considerations provide them with knowledge of how to develop applications using Hadoop with Java and 
-                     finally provide working knowledge of important advanced topics.
-                  </p>
-                  <br>
-                  <h5>Lab Overview : </h5>
-                  <p>The lab is a student tailored project that will bring together the various concepts learned during the course. 
-                     Together with the instructor the student will design a project that can use an existing or one their own 
-                     data sets. This project concept will be determined at the beginning of class, allowing the student to take 
-                     the concepts taught and apply them to a project tailored to their need/liking.
-                  </p>
-                  <br>
-                  <h5>Prerequisite &amp; Target Audience : </h5>
-                  <p>This course is designed for Developers, students and technical managers who want to learn to implement 
-                     Hadoop MapReduce jobs or manage Big data projects. The course is ideal for experienced software developers 
-                     who have experience in traditional data management technologies and want to upgrade their skills.
-                  </p>
-                  <br>
-                  <h5>Duration : </h5>
-                  <p>4-5 Days OR 32-40 Hours</p>
-                  <br>
-                  <h5>Price : </h5>
-               </div>
-            </div>
+   
+         
+   <div class="panel panel-default">
+      <div class="panel-heading">
+         <h4 class="panel-title">
+            <a data-toggle="collapse" data-parent="#accordion" href="#datascience">
+            DATA SCIENCE
+            </a>
+         </h4>
+         <div class="trainingNavRight">
+            <a href="#"><span><i class="fa fa-pencil-square-o"></i>Register</span></a>
+            <a href="files/Data Science Course.pdf" target="_blank"><span><i class="fa fa-list "></i>Table of Contents</span></a>
+            <a data-toggle="modal" href="#devloperInstructor"><span><i class="fa fa-user"></i>Meet the Instructor</span></a>
+         </div>
+      </div>
+      <div id="datascience" class="panel-collapse collapse in">
+         <div class="panel-body">
+            <h5>Course Overview : </h5>
+            <p>Complete overview of Hadoop, it’s components and implementation details. This course will go into 
+               more detail and be tailored more to the real world than other Hadoop course offered. The instruction 
+               is led by seasoned software engineers who have experience developing real work Big Data projects.
+            </p>
+            <br>
+            <h5>Objectives : </h5>
+            <p>Goal is for the student to gain a thorough understanding of the Hadoop ecosystem, basic operational 
+               considerations provide them with knowledge of how to develop applications using Hadoop with Java and 
+               finally provide working knowledge of important advanced topics.
+            </p>
+            <br>
+            <h5>Lab Overview : </h5>
+            <p>The lab is a student tailored project that will bring together the various concepts learned during the course. 
+               Together with the instructor the student will design a project that can use an existing or one their own 
+               data sets. This project concept will be determined at the beginning of class, allowing the student to take 
+               the concepts taught and apply them to a project tailored to their need/liking.
+            </p>
+            <br>
+            <h5>Prerequisite &amp; Target Audience : </h5>
+            <p>This course is designed for Developers, students and technical managers who want to learn to implement 
+               Hadoop MapReduce jobs or manage Big data projects. The course is ideal for experienced software developers 
+               who have experience in traditional data management technologies and want to upgrade their skills.
+            </p>
+            <br>
+            <h5>Duration : </h5>
+            <p>4-5 Days OR 32-40 Hours</p>
+            <br>
+            <h5>Price : </h5>
          </div>
       </div>
    </div>
